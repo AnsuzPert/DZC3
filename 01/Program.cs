2091f22b05d8cc53e7f40cc4759098b55facba35
unknown <ansuzpert88@gmail.com>
2022-09-01 18:11:50 +0300

    Первое задание

---
+++ 01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("ВВедите целое пятизначное число");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x/10000==x%10) 
+{if ((x/1000%10)==(x%100/10)) Console.WriteLine("да");
+else Console.WriteLine("нет");
+}
+else Console.WriteLine("нет");
